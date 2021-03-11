c34be1c03721364d1a568a8808386f01ce653c12
Wesley Stam <wstam88@gmail.com>
2021-03-11 16:58:28 +0100

    Removed example folder and template references

---
+++ template/default/winfu.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: '{{name}}',
+    description: '{{description}}',
+    icon: '/icon.png',
+    entries: [
+      {
+        label: 'ExampleComponent',
+        component: 'ExampleComponent'
+      }
+    ]
+  }
+  
